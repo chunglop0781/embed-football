fe265717ad6e692bcf2448ad6e54ff3fd13f75fb
chunglop0781 <chunglop0781@gmail.com>
2019-07-21 20:26:17 +0800

    Update xoilac13.php

--- xoilac13.php
+++ xoilac13.php
@@ -1,1 +1,1 @@
-https://cdn9.90phut.tv/add30bccea9eac6fcbf5f81566e0efbb06f02fdc/849a0c5635df4f4ebd0eeab554c44b17/1563754870/app_1_13e88/ch1/chunklist.m3u8
+https://cdn9.90phut.tv/add30bccea9eac6fcbf5f81566e0efbb06f02fdc/849a0c5635df4f4ebd0eeab554c44b17/1563754870/app_1_13e88/ch1/playlist.m3u8
