c804b728b70680bc632bb1055b472bc6d379ec66
chunglop0781 <chunglop0781@gmail.com>
2019-07-24 19:50:19 +0800

    Update xoilac12.php

--- xoilac12.php
+++ xoilac12.php
@@ -1,1 +1,1 @@
-https://cdn9.90phut.tv/af9ad8c2ff117527dd95d16f2500036ab6dc06b1/849a0c5635df4f4ebd0eeab554c44b17/1563754900/app_1_13e88/ch1/playlist.m3u8
+https://cdn.90phut.tv/live69/juveSD/playlist.m3u8
